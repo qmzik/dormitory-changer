00aa343e0b6875d3e1a408ea5de4a10981dad375
qmzik <nikita.lebedev20812@gmail.com>
2019-11-11 22:47:53 +0500

    Сокет тестирование

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
 import http from 'http';
 import app from './app';
+import socketIo from 'socket.io';
+
 
 const port = 8000;
 
 const server = http.createServer(app);
 
 server.listen(port);
+
+export default server;
+
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('connected');
+});
