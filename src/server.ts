3eb16efe5971f62d3e274f848959d886794e0529
qmzik <qmzik208@gmail.com>
2019-11-23 11:21:37 +0500

    удалён сокет

--- src/server.ts
+++ src/server.ts
@@ -1,60 +1,8 @@
 import http from 'http';
 import app from './app';
-import socketIo from 'socket.io';
-import jwt from 'jsonwebtoken';
-import HttpError from './HttpError';
 
 const port = 8000;
 
 const server = http.createServer(app);
 
 server.listen(port);
-
-const io = socketIo(server);
-
-const connectedUsers: Map<number, string> = new Map<number, string>();
-
-io.use((socket, next) => {
-    try {
-        const token = socket.handshake.query.token;
-        const userId = socket.handshake.query.userId;
-        if (!userId) {
-            socket.disconnect();
-        }
-
-        jwt.verify(token, process.env.SECRET);
-
-        next();
-    } catch (error) {
-        socket.disconnect();
-    }
-});
-
-io.on('connection', (socket) => {
-    const socketId = socket.id;
-    const userId = socket.handshake.query.userId;
-
-    connectedUsers.set(userId, socketId);
-
-    socket.on('msg', (event: IMessage) => {
-        const msg: ISocketMessage = { status: 200, message: 'Sending message', payload: event };
-        
-        io.to(connectedUsers.get(event.to)).emit('msg', msg);
-    });
-
-    socket.on('disconnect', () => {
-        connectedUsers.delete(userId);
-    });
-});
-
-interface IMessage {
-    from: number;
-    to: number;
-    msg: string;
-}
-
-interface ISocketMessage {
-    status: number;
-    message: string;
-    payload?: any;
-}
